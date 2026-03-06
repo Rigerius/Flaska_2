5090ff7d7de413a3fc300e7423767362f3fff341
Rigerius <mike.bklv@mail.ru>
2026-03-06 19:09:54 +0300

    Update фласка_2_4.py

--- Автоматический ответ/фласка_2_4.py
+++ Автоматический ответ/фласка_2_4.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/list_prof/<list>')
-def profile(list):
-    return render_template('index_4.html', list=list)
+@app.route('/answer')
+@app.route('/auto_answer')
+def profile():
+    return render_template('index_4.html')
 
 
 if __name__ == '__main__':
-
     app.run(port=8080, host='127.0.0.1')
+    
